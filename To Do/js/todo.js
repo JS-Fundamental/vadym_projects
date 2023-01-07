8aadbd49dcdc46f0b73b96363d48515f9fd2bf2b
Vadym Shatnii <vadymshatniy@gmail.com>
2023-01-07 22:09:26 +0200

    Todo start
    Start of the collective task "To Do"

---
+++ To Do/js/todo.js
@@ -0,0 +1,39 @@
+
+
+
+document.querySelector('.add').addEventListener('click', () => { 
+    console.log('hello')
+})
+
+/*
+document.querySelector('.add').addEventListener('click', () => {
+    let labourTitle = document.querySelector('.input-title').value;
+    let labourDescription = document.querySelector('.input-description').value;
+    laboursList[0] = labourTitle;
+    laboursList[1] = labourDescription;
+})
+*/
+
+    //document.querySelector('.list-titles').innerHTML = labourTitle;
+    //document.querySelector('.list-descriptions').innerHTML = labourDescription;
+
+
+
+
+/*document.querySelector('.add').addEventListener('click', () => {
+    let data = document.querySelector('.input-title').value;
+    document.querySelector('.list-titles').innerHTML = data;
+})
+*/
+/*
+//Створюється конструктор тудушки:
+function Todo(title, description, time, status) {
+    this.title = title;
+    this.description = description;
+    this.time = time;
+    this.status = status;
+}
+
+//Функція створення першої тудушки:
+let firstTodo = new Todo("first title", "first description", "first time", "first status");
+*/
